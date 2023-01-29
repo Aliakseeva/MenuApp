6dea4c141a390e21d000682c01f0530c3b1942d4
Aliakseeva <elizavetaz0902@gmail.com>
2023-01-29 21:08:10 +0300

    Fix: migration, Set up: pre-commit file

--- MenuApp/migrations/env.py
+++ MenuApp/migrations/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-from src.models import metadata
+from src.models import *
 
 # sys.path = ['', '..'] + sys.path[1:]
 
@@ -28,7 +28,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
